f23b68c87f1f4626b60e86f540d964b6924ea8de
Muhannad <muhannad_2006@hotmail.com>
2020-06-16 13:53:30 -0600

    add Rust implementation of Unix "ls" command

---
+++ ls/src/main.rs
@@ -0,0 +1,20 @@
+use std::{fs, io};
+
+fn main() -> io::Result<()> {
+    let mut entries = fs::read_dir(".")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    // The order in which `read_dir` returns entries is not guaranteed. If reproducible
+    // ordering is required the entries should be explicitly sorted.
+
+    entries.sort();
+
+    // The entries have now been sorted by their path.
+
+    for x in 0..entries.len() {
+        println!("{}", entries[x].display());
+    }
+
+    Ok(())
+}
